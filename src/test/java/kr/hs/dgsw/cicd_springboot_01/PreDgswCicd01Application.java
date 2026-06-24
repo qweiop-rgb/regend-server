5ac149ec71bade71394f06efdfa72e76267d8f2f
hongdu08 <kimdh04150416@gmail.com>
2026-06-24 09:57:05 +0900

    test exception

---
+++ src/test/java/kr/hs/dgsw/cicd_springboot_01/PreDgswCicd01Application.java
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.cicd_springboot_01;
+
+import org.junit.jupiter.api.Test;
+
+public class PreDgswCicd01Application {
+    @Test
+    void contextLoads() {
+        throw new RuntimeException("Runtime Exception");
+    }
+}
